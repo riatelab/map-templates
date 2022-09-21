9ca06ba206ca71ef735ba9e1212fd733194f7a70
rysebaert <ronan.ysebaert@cnrs.fr>
2022-09-21 14:42:50 +0200

    feat: a function to manage the main frame of the EU template

--- EU_template.R
+++ EU_template.R
@@ -1,4 +1,9 @@
-devtools::install_github("riatelab/mapinsetr")
+#########################
+# European template
+#########################
+
+
+#devtools::install_github("riatelab/mapinsetr")
 library(sf)
 library(giscoR)
 library(mapsf)
@@ -7,15 +12,53 @@ library(mapinsetr)
 frame <- st_read("input/eu/frame.geojson")
 frame <- st_transform(frame, 3035)
 
-# Main frame
+# Main frame ----
+main_frame <- function(template, frame, level, up_units = NULL, res){
+  if(template == "europe"){
+    units <- gisco_get_nuts(year = "2021", epsg = "3035", resolution = res, 
+                           nuts_level = level)
+    
+    neigbours <- gisco_get_countries(year = "2020", epsg = "3035", 
+                                     resolution = res)
+    
+    urban_audit <- gisco_get_urban_audit(year = "2020", epsg = "3035", 
+                                         spatialtype = "LB")
+    
+    st_agr(nuts) = "constant"
+    st_agr(countries) = "constant"
+    st_agr(frame) = "constant"
+    
+    nuts <- st_intersection(nuts, frame)
+    countries <- st_intersection(countries, frame)
+    urban_audit <- st_intersection(urban_audit, frame)
+    
+    # Get borders
+    borders <- st_intersection(st_buffer(countries, 5), st_buffer(countries, 5)) 
+    borders <- st_cast(borders,"MULTILINESTRING")
+    borders <- borders[borders$CNTR_ID != borders$CNTR_ID.1, ] 
+    borders <- borders[,(1:length(countries))]
+    
+    return(list("units" = nuts, "neighbours" = countries, "borders" = borders,
+           "cities" = urban_audit))
+  }
+}  
+  
 
+# Test
+europe <- main_frame(template = "europe", frame = frame, level = "2", res = "20")
 
-nuts <- gisco_get_nuts(year = "2021", epsg = "3035", resolution = "20", nuts_level = "2")
-bound <- gisco_get_nuts(year = "2021", epsg = "3035", resolution = "20", nuts_level = "0", spatialtype = "BN")
-nuts <- st_intersection(nuts, frame)
+nuts <- europe$units
+countries <- europe$neighbours
+borders <- europe$borders
+cities <- europe$cities
 
-mf_map(nuts)
-mf_map(bound, col = "red", add = TRUE)
+# Plot
+mf_map(frame, col = "lightblue")
+mf_map(countries, col = "grey", border = NA, add = TRUE)
+mf_map(nuts, col = "peachpuff", border = "grey", add = TRUE)
+mf_map(borders, col = "white", lwd = 1, add = TRUE)
+mf_map(cities, pch = 21, bg = "red", cex = .6, add = TRUE)
+
 
 
 
@@ -38,12 +81,11 @@ boxes$target <- list(c(-17.36,32.3,-16.17,33.1), # OK
                      c(54.8,-21.5,56.3,-20.5),
                      c(-66,-6.3,-39,12.5),
                      c(-28.088, 36.662, -24.576, 39.22))
-
 boxes$epsg_loc <- c(2191, 4471, 5490, 5490, 3035, 3063, 2975, 2972, 3063)
-boxes$epsg_out <- rep(3035, 9)
+boxes$res <- c("20", "20")
 boxes$k <- c(2, 5, 3.5, 4.2, 0.72, 2, 4.5, 0.85, 2)
 
-epsg_out <- 3035
+epsg_out <- st_crs(frame)
 
 nuts <- gisco_get_nuts(year = "2021", epsg = "4326", resolution = "10", nuts_level = "2")
 countries <- gisco_get_countries(year = "2020", epsg = "4326", resolution = "3")
@@ -95,6 +137,7 @@ x <- x[-1,]
 
 if (cp){x <- st_cast(x, "MULTIPOLYGON")}
 st_crs(x) <- prj
+
 
 # names order mngmt
 x <- x[, namesorder]
