676ded15e6b317a63e6c16352a3bd5a7536884d2
rysebaert <ronan.ysebaert@cnrs.fr>
2022-09-22 19:24:25 +0200

    feat : add function to manage boxes and add programme to build nice boxes for EU template

--- EU_template.R
+++ EU_template.R
@@ -2,15 +2,25 @@
 # European template
 #########################
 
-
 #devtools::install_github("riatelab/mapinsetr")
 library(sf)
 library(giscoR)
 library(mapsf)
 library(mapinsetr)
 
-frame <- st_read("input/eu/frame.geojson")
-frame <- st_transform(frame, 3035)
+# Set frame parameters
+xmin <- 2380000
+xmax <- 6550000
+ymin <- 1350000
+ymax <- 5420000
+
+lon <- c(xmin,xmax,xmax,xmin,xmin)
+lat <- c(ymin,ymin,ymax,ymax,ymin)
+
+frame <- st_sfc(st_polygon(list(cbind(lon, lat))))
+frame <- st_as_sf(frame)
+st_crs(frame) <- 3035
+
 
 # Main frame ----
 main_frame <- function(template, frame, level, up_units = NULL, res){
@@ -18,22 +28,19 @@ main_frame <- function(template, frame, level, up_units = NULL, res){
     units <- gisco_get_nuts(year = "2021", epsg = "3035", resolution = res, 
                            nuts_level = level)
     
-    neigbours <- gisco_get_countries(year = "2020", epsg = "3035", 
+    neighbours <- gisco_get_countries(year = "2020", epsg = "3035", 
                                      resolution = res)
     
     urban_audit <- gisco_get_urban_audit(year = "2020", epsg = "3035", 
                                          spatialtype = "LB")
     
-    st_agr(nuts) = "constant"
-    st_agr(countries) = "constant"
-    st_agr(frame) = "constant"
-    
-    nuts <- st_intersection(nuts, frame)
-    countries <- st_intersection(countries, frame)
-    urban_audit <- st_intersection(urban_audit, frame)
+    nuts <- suppressWarnings(st_intersection(units, st_geometry(frame)))
+    countries <- suppressWarnings(st_intersection(neighbours, st_geometry(frame)))
+    urban_audit <- suppressWarnings(st_intersection(urban_audit, st_geometry(frame)))
     
     # Get borders
-    borders <- st_intersection(st_buffer(countries, 5), st_buffer(countries, 5)) 
+    borders <- suppressWarnings(st_intersection(st_buffer(countries, 5), 
+                                                st_buffer(countries, 5))) 
     borders <- st_cast(borders,"MULTILINESTRING")
     borders <- borders[borders$CNTR_ID != borders$CNTR_ID.1, ] 
     borders <- borders[,(1:length(countries))]
@@ -42,7 +49,133 @@ main_frame <- function(template, frame, level, up_units = NULL, res){
            "cities" = urban_audit))
   }
 }  
+
+
+box_move_and_resize <- function(boxes, x, x_target){
   
+  for (i in 1:nrow(boxes)){
+    box <- boxes[i,]
+
+    # Create mask polygon from input parameters
+    lon <- as.vector(unlist(box[,"target"])[c(1,3,3,1,1)])
+    lat <- as.vector(unlist(box[,"target"])[c(2,2,4,4,2)])
+    mask <- st_sfc(st_polygon(list(cbind(lon, lat))))
+    st_crs(mask) <- st_crs(x)
+    
+    # if pts 
+    if (class(st_geometry(x))[1]=="sfc_POINT"){
+      x <- ptbox_move_and_resize(x = x, mask = mask, xy = xy, prj = prj, k = k)
+      return(x)
+    }
+    
+    # names order mngmt
+    namesorder <- names(x)
+    
+    # multipolygon mgmt
+    cp <- class(st_geometry(x))[1]=="sfc_MULTIPOLYGON"
+    
+    # Intersect with mask
+    sel <- suppressWarnings(st_intersection(x, st_geometry(mask))) 
+
+    # add mask to x
+    xm <- sel[1, ]
+    st_geometry(xm) <- st_geometry(mask)
+    sel <- rbind(xm, sel)
+
+    # Transform 
+    sel <- st_transform(sel, box[,"epsg_loc", drop = T])
+    
+    # Move to the center of the box
+    xy <- suppressWarnings(st_centroid(box))
+    xy <- st_bbox(xy)[1:2]
+    
+    cntrd <- st_centroid(st_combine(sel))
+    xg <- (st_geometry(sel) - cntrd) + cntrd[[1]][] 
+    st_geometry(sel) <- xg + xy - (st_bbox(st_centroid(xg))[1:2])
+    
+    # Resize
+    bbox_ref <- st_bbox(box) # Bbox in the template
+    bbox_input <- st_bbox(sel[1,]) # Bbox of the selection
+    
+    x_dif <- (bbox_ref[3] - bbox_ref[1]) / (bbox_input[3] - bbox_input[1]) # Diff in X  
+    y_dif <- (bbox_ref[4] - bbox_ref[2]) / (bbox_input[4] - bbox_input[2]) # diff in Y   
+    k_min <- min(x_dif, y_dif) # Target resize ratio
+    
+    cntrd <- st_centroid(st_combine(sel))
+    xg <- (st_geometry(sel) - cntrd) * k_min + cntrd[[1]][] 
+    st_geometry(sel) <- xg + xy - (st_bbox(st_centroid(xg))[1:2])
+    
+    # get rid of mask
+    sel <- sel[-1,]
+    
+    if (cp){sel <- st_cast(sel, "MULTIPOLYGON")}
+    st_crs(sel) <- st_crs(x_target)
+    
+    # names order mngmt / return nothing if no territories included in the box
+    if(nrow(sel) > 0){
+      sel <- sel[, namesorder]
+      x_target <- rbind(x_target, sel)
+    }
+  }
+return(x_target)
+}
+
+
+
+
+ptbox_move_and_resize <- function(boxes, x, x_target){
+  # names order mngmt
+  namesorder <- names(x)
+  
+  # intersect mask and x
+  x <- suppressWarnings(
+    st_collection_extract(
+      st_intersection(x, st_geometry(mask)),
+      type = c("POINT")
+    )
+  )
+  
+  # add mask to x
+  xm <- x[1, ]
+  st_geometry(xm) <- st_geometry(mask)
+  x <- rbind(xm,x)
+  
+  # Move to the center of the box
+  xy <- suppressWarnings(st_centroid(tmp))
+  xy <- st_bbox(xy)[1:2]
+  
+  cntrd <- st_centroid(st_combine(x))
+  xg <- (st_geometry(x) - cntrd) + cntrd[[1]][] 
+  st_geometry(x) <- xg + xy - (st_bbox(st_centroid(xg))[1:2])
+  
+  # Resize
+  bbox_ref <- st_bbox(box) # Bbox in the template
+  bbox_input <- st_bbox(x[1,]) # Bbox of the selection
+  
+  x_dif <- (bbox_ref[3] - bbox_ref[1]) / (bbox_input[3] - bbox_input[1]) # Diff in X  
+  y_dif <- (bbox_ref[4] - bbox_ref[2]) / (bbox_input[4] - bbox_input[2]) # diff in Y   
+  k_min <- min(x_dif, y_dif) # Target resize ratio
+  
+  cntrd <- st_centroid(st_combine(x))
+  xg <- (st_geometry(x) - cntrd) * k_min + cntrd[[1]][] 
+  st_geometry(x) <- xg + xy - (st_bbox(st_centroid(xg))[1:2])
+  
+  
+  # get rid of mask
+  x <- x[-1,]
+  st_crs(x) <- prj
+  
+  # names order mngmt
+  x <- x[, namesorder]
+  
+  if(nrow(x > 0)){
+    x <- x[, namesorder]
+    
+    x_target <- rbind(x_target, x)}
+
+}
+
+
 
 # Test
 europe <- main_frame(template = "europe", frame = frame, level = "2", res = "20")
@@ -61,93 +194,93 @@ mf_map(cities, pch = 21, bg = "red", cex = .6, add = TRUE)
 
 
 
-
-
-
-
-
-
+# Import target boxes
 boxes <- st_read("input/eu/boxes.geojson")
 
 boxes <- st_transform(boxes, 3035)
 boxes$name <- c("Madeire", "Mayotte", "Martinique", "Guadeloupe", "Canaries",
                 "Açores - Flores", "Réunion", "Guyane", "Açores")
-boxes$target <- list(c(-17.36,32.3,-16.17,33.1), # OK
-                     c(44.95,-13.03,45.357,-12.59), # OK
-                     c(-61.28,14.32,-60.76,14.93), 
-                     c(-61.89,15.738,-60.86,16.602),
-                     c(-20, 25.4,- 10, 30.3),
-                     c(-31.397,39.215,-30.898,39.907),
+boxes$target <- list(c(-17.35, 32.55, -16.2, 33.2), # OK
+                     c(45, -13.02, 45.28, -12.65), # OK
+                     c(-61.28, 14.35, -60.76, 14.93), # OK
+                     c(-61.89, 15.8, -61.15, 16.55), # OK
+                     c(-18.4, 27.4,- 13.3, 30.3), # OK
+                     c(-31.4, 39.3, -30.9, 39.8), # OK
                      c(54.8,-21.5,56.3,-20.5),
                      c(-66,-6.3,-39,12.5),
                      c(-28.088, 36.662, -24.576, 39.22))
 boxes$epsg_loc <- c(2191, 4471, 5490, 5490, 3035, 3063, 2975, 2972, 3063)
-boxes$res <- c("20", "20")
-boxes$k <- c(2, 5, 3.5, 4.2, 0.72, 2, 4.5, 0.85, 2)
+
+
+# Import layer with territorial units to be included in the boxes / boxes$target prj must fit with proj of input layers
+input <- gisco_get_nuts(year = "2021", epsg = "4326", resolution = "20", nuts_level = "2")
+countries <- gisco_get_countries(year = "2020", epsg = "4326", resolution = "20")
+urban_audit <- gisco_get_urban_audit(year = "2020", epsg = "4326", 
+                                     spatialtype = "LB")
+head(bbox)
+
+i <- 7
+box <- boxes[i,]
+lon <- as.vector(unlist(box[,"target"])[c(1,3,3,1,1)])
+lat <- as.vector(unlist(box[,"target"])[c(2,2,4,4,2)])
+mask <- st_sfc(st_polygon(list(cbind(lon, lat))))
+
+mf_map(toto)
+mf_map(input, add = TRUE)
+
+head(input)
+toto <- box_move_and_resize(boxes = boxes, x = input, x_target = nuts) 
+
+head(toto)
+mf_map(toto)
+boxes$geometry
+
+
+i <- 6
+st_coordinates(boxes[i,])
+xmin <- st_coordinates(boxes[i,])[3,1]
+xmax <- st_coordinates(boxes[i,])[1,1]
+ymin <- st_coordinates(boxes[i,])[2,2]
+ymax <- st_coordinates(boxes[i,])[1,2]
+
+i <- 9
+st_coordinates(boxes[i,])
+xmin2 <- st_coordinates(boxes[i,])[1,1]
+xmax2 <- st_coordinates(boxes[i,])[4,1]
+
+
+(xmax - xmin) / (xmax2 - xmin2) 
+
+mf_map(nuts)
+mf_map(boxes[9,], add = T, col = "red")
+
+xmax - xmin
+ymax - ymin
+
+i <- 3
+ymax2 <- st_coordinates(boxes[i,])[1,2]
+ymin - ymax2
+
+locator()
+
+st_coordinates(boxes[3,])
+
+mart_x <- c(6043093, 5818604)
+mart_y <- c(4720499, 4497906)
+diff <- mart_x[1] - mart_x[2]
+diff2 <- mart_y[1] - mart_y[2]
+
+mf_map(toto)
+
+
+#boxes$k <- c(2, 5, 3.5, 4.2, 0.72, 2, 4.5, 0.85, 2)
 
 epsg_out <- st_crs(frame)
 
-nuts <- gisco_get_nuts(year = "2021", epsg = "4326", resolution = "10", nuts_level = "2")
-countries <- gisco_get_countries(year = "2020", epsg = "4326", resolution = "3")
+mf_map(x_target)
 
-
-st_agr(nuts) = "constant"
-st_agr(countries) = "constant"
-
-tmp <- boxes[3,]
+tmp <- boxes[1,]
 bbox <- st_bbox(tmp)
 
 x <- nuts
-prj <- st_crs(bbox)
-
-# Create mask polygon from input parameters
-lon <- as.vector(unlist(tmp[,"target"])[c(1,3,3,1,1)])
-lat <- as.vector(unlist(tmp[,"target"])[c(2,2,4,4,2)])
-mask <- st_sfc(st_polygon(list(cbind(lon, lat))))
-st_crs(mask) <- st_crs(nuts)
-
-# names order mngmt
-namesorder <- names(x)
-
-# multipolygon mgmt
-cp <- class(st_geometry(nuts))[1]=="sfc_MULTIPOLYGON"
-
-# Intersect with mask
-x <- suppressWarnings(st_intersection(nuts, st_geometry(mask))) 
-
-# add mask to x
-xm <- x[1, ]
-st_geometry(xm) <- st_geometry(mask)
-x <- rbind(xm,x)
-
-# Transform 
-x <- st_transform(x, tmp$epsg_loc)
-
-# resize & move
-k <- as.vector(st_set_geometry(tmp[,"k"], NULL))
-xy <- c(bbox[1], bbox[2])
-
-cntrd <- st_centroid(st_combine(x))
-xg <- (st_geometry(x) - cntrd) * k + cntrd[[1]][] # Resize
-st_geometry(x) <- xg + xy - st_bbox(xg)[1:2]
-
-
-# get rid of mask
-x <- x[-1,]
-
-if (cp){x <- st_cast(x, "MULTIPOLYGON")}
-st_crs(x) <- prj
-
-
-# names order mngmt
-x <- x[, namesorder]
-
-
-mf_map(frame)
-
-mf_map(boxes,  add = TRUE)
-mf_map(x, add = TRUE)
-
-mf_map(boxes[9,], col = "red", add = TRUE)
-mf_map(xg, add = TRUE)
-xg
+x_target <- europe$units
---
+++ build_eu_box.R
@@ -0,0 +1,135 @@
+
+# Guadeloupe
+xmin <- 5900000
+xmax <- xmin + 280000
+
+ymax <- 4900000
+ymin <- ymax - 280000
+
+lon <- c(xmin,xmax,xmax,xmin,xmin)
+lat <- c(ymin,ymin,ymax,ymax,ymin)
+
+x <- st_sfc(st_polygon(list(cbind(lon, lat))))
+x <- st_as_sf(x)
+x$id <- 1
+x$name <- "Guadeloupe (FR)"
+
+
+# Martinique
+xmin <- xmax + 43000
+xmax <- xmin + 280000
+lon <-  c(xmin,xmax,xmax,xmin,xmin)
+
+xx <- st_sfc(st_polygon(list(cbind(lon, lat))))
+xx <- st_as_sf(xx)
+xx$id <- 2
+xx$name <- "Martinique (FR)"
+x <- rbind(x, xx)
+
+
+# Réunion
+xmin <- 5900000
+xmax <- xmin + 280000
+ymax <- ymin - 43000
+ymin <- ymax - 280000
+
+lon <- c(xmin,xmax,xmax,xmin,xmin)
+lat <- c(ymin,ymin,ymax,ymax,ymin)
+
+xx <- st_sfc(st_polygon(list(cbind(lon, lat))))
+xx <- st_as_sf(xx)
+xx$id <- 3
+xx$name <- "Réunion (FR)"
+x <- rbind(x, xx)
+
+# Guyane
+xmin <- xmax + 43000
+xmax <- xmin + 280000
+lon <-  c(xmin,xmax,xmax,xmin,xmin)
+
+xx <- st_sfc(st_polygon(list(cbind(lon, lat))))
+xx <- st_as_sf(xx)
+xx$id <- 4
+xx$name <- "Guyane (FR)"
+x <- rbind(x, xx)
+
+# Mayotte
+xmin <- 5900000
+xmax <- xmin + 280000
+ymax <- ymin - 43000
+ymin <- ymax - 280000
+
+lon <- c(xmin,xmax,xmax,xmin,xmin)
+lat <- c(ymin,ymin,ymax,ymax,ymin)
+
+xx <- st_sfc(st_polygon(list(cbind(lon, lat))))
+xx <- st_as_sf(xx)
+xx$id <- 5
+xx$name <- "Mayotte (FR)"
+x <- rbind(x, xx)
+
+# Canaries
+xmin <- xmax + 43000
+xmax <- xmin + 280000
+lon <-  c(xmin,xmax,xmax,xmin,xmin)
+
+xx <- st_sfc(st_polygon(list(cbind(lon, lat))))
+xx <- st_as_sf(xx)
+xx$id <- 6
+xx$name <- "Canaries (ES)"
+x <- rbind(x, xx)
+
+# Madeire
+xmin <- 5900000
+xmax <- xmin + 280000
+ymax <- ymin - 43000
+ymin <- ymax - 280000
+
+lon <- c(xmin,xmax,xmax,xmin,xmin)
+lat <- c(ymin,ymin,ymax,ymax,ymin)
+
+xx <- st_sfc(st_polygon(list(cbind(lon, lat))))
+xx <- st_as_sf(xx)
+xx$id <- 7
+xx$name <- "Madeire (PT)"
+x <- rbind(x, xx)
+
+
+# Açores (main)
+xmin <- xmax + 43000
+xmax <- xmin + 280000
+lon <-  c(xmin,xmax,xmax,xmin,xmin)
+
+xx <- st_sfc(st_polygon(list(cbind(lon, lat))))
+xx <- st_as_sf(xx)
+xx$id <- 8
+xx$name <- "Açores (PT)"
+x <- rbind(x, xx)
+
+
+# Açores (second)
+xmax <- xmin + (.3 * (xmax - xmin))
+ymin <- ymax - (.3 * (ymax - ymin))
+
+lon <- c(xmin,xmax,xmax,xmin,xmin)
+lat <- c(ymin,ymin,ymax,ymax,ymin)
+
+xx <- st_sfc(st_polygon(list(cbind(lon, lat))))
+xx <- st_as_sf(xx)
+xx$id <- 9
+xx$name <- "Açores (Florès - PT)"
+x <- rbind(x, xx)
+
+boxes <- x
+boxes$target <- list(c(-61.89, 15.8, -61.15, 16.55),
+                     c(-61.28, 14.35, -60.76, 14.93),
+                     c(54.8,-21.5,56.3,-20.5),
+                     c(-66,-6.3,-39,12.5), 
+                     c(45, -13.02, 45.28, -12.65),
+                     c(-18.4, 27.4,- 13.3, 30.3),
+                     c(-17.35, 32.55, -16.2, 33.2),
+                     c(-28.088, 36.662, -24.576, 39.22),
+                     c(-31.4, 39.3, -30.9, 39.8))
+                     
+boxes$epsg_loc <- c(5490, 5490, 2975, 2972, 4471, 3035, 2191, 3063, 3063)
+head(boxes)
