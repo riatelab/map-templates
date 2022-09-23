6af8e5a4272b626dbd15b006eacec45212742b12
Ronan Ysebaert <ronan.ysebaert@cnrs.fr>
2022-09-23 09:31:13 +0200

    feat: add a function to get predicted k in box layer

--- EU_template.R
+++ EU_template.R
@@ -6,7 +6,7 @@
 library(sf)
 library(giscoR)
 library(mapsf)
-library(mapinsetr)
+#library(mapinsetr)
 
 # Set frame parameters
 xmin <- 2380000
@@ -121,8 +121,6 @@ return(x_target)
 }
 
 
-
-
 ptbox_move_and_resize <- function(boxes, x, x_target){
   # names order mngmt
   namesorder <- names(x)
@@ -174,6 +172,54 @@ ptbox_move_and_resize <- function(boxes, x, x_target){
     x_target <- rbind(x_target, x)}
 
 }
+
+i <- 1
+box_k <- function(boxes, x, x_target){
+  for (i in 1:nrow(boxes)){
+    box <- boxes[i,]
+    
+    # Create mask polygon from input parameters
+    lon <- as.vector(unlist(box[,"target"])[c(1,3,3,1,1)])
+    lat <- as.vector(unlist(box[,"target"])[c(2,2,4,4,2)])
+    mask <- st_sfc(st_polygon(list(cbind(lon, lat))))
+    st_crs(mask) <- st_crs(x)
+    
+    # multipolygon mgmt
+    cp <- class(st_geometry(x))[1]=="sfc_MULTIPOLYGON"
+    
+    # Intersect with mask
+    sel <- suppressWarnings(st_intersection(x, st_geometry(mask))) 
+    
+    # add mask to x
+    xm <- sel[1, ]
+    st_geometry(xm) <- st_geometry(mask)
+    sel <- rbind(xm, sel)
+    
+    # Transform 
+    sel <- st_transform(sel, box[,"epsg_loc", drop = T])
+    
+    # Move to the center of the box
+    xy <- suppressWarnings(st_centroid(box))
+    xy <- st_bbox(xy)[1:2]
+    
+    cntrd <- st_centroid(st_combine(sel))
+    xg <- (st_geometry(sel) - cntrd) + cntrd[[1]][] 
+    st_geometry(sel) <- xg + xy - (st_bbox(st_centroid(xg))[1:2])
+    
+    # Resize
+    bbox_ref <- st_bbox(box) # Bbox in the template
+    bbox_input <- st_bbox(sel[1,]) # Bbox of the selection
+    
+    x_dif <- (bbox_ref[3] - bbox_ref[1]) / (bbox_input[3] - bbox_input[1]) # Diff in X  
+    y_dif <- (bbox_ref[4] - bbox_ref[2]) / (bbox_input[4] - bbox_input[2]) # diff in Y   
+    k_min <- min(x_dif, y_dif) # Target resize ratio
+    boxes[i, "k"] <- k_min
+  }
+  return(boxes)
+}
+
+  
+
 
 
 
@@ -230,6 +276,7 @@ mf_map(input, add = TRUE)
 
 head(input)
 toto <- box_move_and_resize(boxes = boxes, x = input, x_target = nuts) 
+boxes <- box_k(boxes = boxes, x = input, x_target = nuts)
 
 head(toto)
 mf_map(toto)
